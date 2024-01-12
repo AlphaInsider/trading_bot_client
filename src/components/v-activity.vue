bdcbf93e89124467f93bf15778edb5d3b91a270b
smithb1994 <14811263+smithb1994@users.noreply.github.com>
2024-01-12 09:26:24 -0500

    - updated home page

--- src/components/v-activity.vue
+++ src/components/v-activity.vue
@@ -7,15 +7,14 @@
         <h4 class="text-capitalize text-truncate mb-0">{{ activity.type }}</h4>
       </div>
       <!-- details -->
-      <div class="col-9 col-md-10 d-flex align-items-center px-3 py-4">
-        <div>
+      <div class="col-9 col-md-10 align-items-center px-3 py-4">
+        <!-- labels -->
+        <div class="d-flex">
           <label>Message</label>
-          <h5 class="mb-0">{{ activity.message }}</h5>
+          <label class="text-muted text-nowrap ml-auto"><i class="far fa-history mt-1"></i> <v-date :date="activity.created_at"></v-date></label>
         </div>
-        <!-- Date Created -->
-        <div class="ml-auto">
-          <small class="text-muted text-nowrap ml-auto"><i class="far fa-history mt-1"></i> <v-date :date="activity.created_at"></v-date></small>
-        </div>
+        <!-- message -->
+        <h5 class="mb-0">{{ activity.message }}</h5>
       </div>
     </div>
   </div>
@@ -31,6 +30,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-</style>
